9ed2a1804822a9bd3df26e34f9caf250c36d72d3
zhuchentong <2037630@163.com>
2019-12-24 13:02:47 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,8 @@ const routeExtend = require('./extends/route.extend')
 const svgExtend = require('./extends/svg.extend')
 const i18nExtend = require('./extends/i18n.extend')
 module.exports = {
+  // 资源根路径
+  publicPath: '/vue-web-service/',
   // 修改webpack配置
   chainWebpack: config => {
     // 别名扩展
